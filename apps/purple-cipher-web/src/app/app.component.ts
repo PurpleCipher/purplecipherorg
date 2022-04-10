978276d8d51c77601b0f1cc89cf2958a4e5602a3
Thompson Edolo <verygreenboi@live.com>
2022-04-10 10:22:52 +0100

    feat(workspace): init commit

---
+++ apps/purple-cipher-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'purplecipherorg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'purple-cipher-web';
+}
