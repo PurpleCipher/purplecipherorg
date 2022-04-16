e89315a3b3649d4c3a1b75eabbab99153428c61f
Thompson Edolo <verygreenboi@live.com>
2022-04-16 14:14:26 +0100

    chore: update commit lint rules

---
+++ .commitlintrc.js
@@ -0,0 +1,7 @@
+const typeEnum = require('@commitlint/config-angular-type-enum');
+module.exports = {
+    "extends": ["@commitlint/config-angular"],
+    "rules": {
+        "type-enum": [2, "always", [...typeEnum.value(), 'chore', 'version-bump']]
+    }
+};
