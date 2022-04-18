8052d46b42e0eda9a69ccecf61487c8ce328b6aa
Thompson Edolo <verygreenboi@live.com>
2022-04-18 11:55:48 +0100

    fix: trigger build

--- apps/purple-cipher-web/src/main.ts
+++ apps/purple-cipher-web/src/main.ts
@@ -6,7 +6,7 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
-  // console.error('Production mode enabled');
+  console.error('Production mode enabled');
 }
 
 platformBrowserDynamic()
