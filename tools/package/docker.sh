1deb52bd60d864b78db44765326721f5833deb77
Thompson Edolo <verygreenboi@live.com>
2022-04-18 12:25:28 +0100

    fix: correct image name

--- tools/package/docker.sh
+++ tools/package/docker.sh
@@ -6,8 +6,8 @@ dist=$3
 
 VERSION="$MAJOR.$MINOR.$PATCH"
 echo "Version: $VERSION"
-TAG="$REPO-$appName:$VERSION"
-LATEST="${REPO}-${appName}:latest"
+TAG="ghcr.io/PurpleCipher/$REPO-$appName:$VERSION"
+LATEST="ghcr.io/PurpleCipher/${REPO}-${appName}:latest"
 BUILD_TIMESTAMP=$( date '+%F_%H:%M:%S' )
 
 
